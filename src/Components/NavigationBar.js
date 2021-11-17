9a7b254f91920169faa355f8dd976b562a5e4d23
Cameron Montesano <ckmontesano@gmail.com>
2021-11-17 03:59:21 -0700

    mobile navbar

--- src/Components/NavigationBar.js
+++ src/Components/NavigationBar.js
@@ -1,31 +1,46 @@
 
 import { Link } from 'react-router-dom';
+import { useState } from 'react/cjs/react.development';
 
 import './NavigationBar.css';
 
 function NavigationBar() {
 
+    const [navBarState, changeNavBarState] = useState("collapsed");
+
+    function toggleNavBar() {
+        if ( navBarState == "collapsed") {
+            changeNavBarState("expanded");
+        } else {
+            changeNavBarState("collapsed");
+        }
+    }
+
     return (
-        <div id="navigation-bar">
+        <div id="navigation-bar" className={navBarState}>
             <Link to="/" className="brand-name">
                 <text className="brand">Brand</text>
                 <text>Coffee</text>
             </Link>
+            <div onClick={toggleNavBar} id="expand-collapse-button" className={navBarState}>
+                {/* this button expands or collapses the navigation bar (only shown on mobile) */}
+                <text>x</text>
+            </div>
             <ul className="links">
                 <li>
-                    <Link to="/menu">
+                    <Link onClick={toggleNavBar} to="/menu">
                         Menu
                         <hr/>
                     </Link>
                 </li>
                 <li>
-                    <Link to="/locations">
+                    <Link onClick={toggleNavBar} to="/locations">
                         Locations
                         <hr/>
                     </Link>
                 </li>
                 <li>
-                    <Link to="/our-story">
+                    <Link onClick={toggleNavBar} to="/our-story">
                         Our Story
                         <hr/>
                     </Link>
